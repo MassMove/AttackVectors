4bf6aded40b3ff509b232e12e50669b7696e8cc6
J. Brett Cunningham <brettcu@gmail.com>
2020-03-08 21:52:18 -0400

    initial upload of stuff

--- website-formatter.py
+++ website-formatter.py
@@ -1,14 +1,17 @@
-import csv, sys, socket
+import csv, sys, socket, json
 from ip2geotools.databases.noncommercial import DbIpCity
+from passivetotal.libs.attributes import AttributeRequest
 
 with open(sys.argv[1]) as csvfile:
     filereader = csv.DictReader(csvfile)
     for row in filereader:
+        # Get the IP address of the domain name
         try:
             ip = socket.gethostbyname(row['domain'])
         except:
             ip = "domain did not resolve"
 
+        # Get geo-coordinates of the IP address
         coordinates = None
         if not row['lat'] and not row['lng']:
             try:
@@ -16,7 +19,33 @@ with open(sys.argv[1]) as csvfile:
             except:
                 pass
         
+        # Gather web trackers (Google Analytics Tracking ID, FacebookId, etc.)
+        trackers = ""
+        print(trackers)
+        csv_row = ""
         if coordinates:
-            print(row['domain'] + ", " + str(coordinates.latitude) + ", " + str(coordinates.longitude) + ", " + row['locationVerified'] + ", " + ip)
+            csv_row = row['domain'] + ", " + str(coordinates.latitude) + ", " + str(coordinates.longitude) + ", " + row['locationVerified'] + ", " + ip
         else:
-            print(row['domain'] + ", " + row['lat'] + ", " + row['lng'] + ", " + row['locationVerified'] + ", " + ip)
+            csv_row = row['domain'] + ", " + row['lat'] + ", " + row['lng'] + ", " + row['locationVerified'] + ", " + ip
+        if "GoogleAnalyticsAccountnNumber" in trackers.values():
+            csv_row = csv_row + ", " + trackers['GoogleAnalyticsAccountNumber']
+        else:
+            csv_row = csv_row + ", , "
+        if "GoogleAnalyticsTrackingId" in trackers.values():
+            csv_row = csv_row + ", " + trackers['GoogleAnalyticsTrackingId']
+        else:
+            csv_row = csv_row + ", , "
+        if "AddThisPubID" in trackers.values():
+            csv_row = csv_row + ", " + trackers['AddThisPubId']
+        else:
+            csv_row = csv_row + ", , "
+        if "FacebookId" in trackers.values():
+            csv_row = csv_row + ", " + trackers['FacebookId']
+        else:
+            csv_row = csv_row + ", , "
+        if "TwitterId" in trackers.values():
+            csv_row = csv_row + ", " + trackers['TwitterId']
+        else:
+            csv_row = csv_row + ", , "
+        
+        print(csv_row)
