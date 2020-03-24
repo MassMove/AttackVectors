a84beb54c013c529a5f171a125c3dad1f33c5491
mentor20 <massmove20@gmail.com>
2020-03-24 22:12:03 +0100

    Add util to check if list of domains already exists in sites.csv

---
+++ LocalJournals/utils/ExistingDomainChecker/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.IO;
+
+namespace CoordCopy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is our world now");
+
+            String sitesCsv = File.ReadAllText("../../../../../sites.csv").ToLower();
+
+            String[] domainsToCheck = File.ReadAllText("../../../DomainsToCheck.txt").ToLower().Split(new[] { "\r\n" }, StringSplitOptions.None);
+
+            foreach (String domainToCheck in domainsToCheck)
+            {
+                if (!sitesCsv.Contains(domainToCheck))
+                {
+                    File.AppendAllText("../../../DomainsToAdd.txt", domainToCheck + "\r\n");
+                    Console.WriteLine("Add " + domainToCheck);
+                }
+            }
+        }
+    }
+}
