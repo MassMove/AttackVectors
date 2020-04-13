665161ce6c575aed517b91c28811f822c63e342a
IAmCoder <mcoder@lucid-code.com>
2020-04-13 16:40:05 +0200

    Tweak cyber dome code - wait 10 minutes between posts

--- LocalJournals/utils/CyberDome/tron.py
+++ LocalJournals/utils/CyberDome/tron.py
@@ -2,10 +2,11 @@ import os
 import praw
 import csv
 from datetime import datetime, timedelta
+import time
 
 reddit = praw.Reddit(client_id='',
                      client_secret="", password='',
-                     user_agent='', username='')
+                     user_agent='CYBERDOME', username='Tron_I_Fight_4_Users')
 
 response = "User..."
 
@@ -14,13 +15,12 @@ with open("reply.txt", "r") as f:
         
 while (True):
     yesterday = datetime.utcnow() - timedelta(days=1)
+    print(datetime.utcnow())
     with open("../../sites.csv") as csvfile:
         filereader = csv.DictReader(csvfile)
         for row in filereader:
             
             posts = reddit.domain(row["domain"]).new(limit=10)
-            
-            print("domain", row["domain"], len(list(posts)))
             
             # Have we run this domain before? If not, create an empty list
             if not os.path.isfile("domains/" + row["domain"] + ".txt"):
@@ -28,7 +28,7 @@ while (True):
             # If we have run the code before, load the list of posts we have replied to
             else:
                 # Read the file into a list
-                with open("posts_replied_to.txt", "r") as f:
+                with open("domains/" + row["domain"] + ".txt", "r") as f:
                     posts_replied_to = f.read()
                     posts_replied_to = posts_replied_to.split("\n")
                     posts_replied_to = list(filter(None, posts_replied_to))
@@ -39,17 +39,17 @@ while (True):
                     post_date = datetime.fromtimestamp(post.created_utc)
                     
                     if post_date > yesterday:
-                    
-                        print("  post", "date", post_date, "title", title)
-                    
                         # Reply to the post
-                        submission.reply(response)
-                        print("Replying to : ", post.author, post.permalink, post.title)
+                        print("Replying to : ", post.id, post.author, post.permalink, post_date)
+                        post.reply(response)
 
                         # Store the current id into our list
                         posts_replied_to.append(post.id)
         
-            # Write our updated list back to the file
-            with open("domains/" + row["domain"] + ".txt", "w") as f:
-                for post_id in posts_replied_to:
-                    f.write(post_id + "\n")
+                        # Write our updated list back to the file
+                        with open("domains/" + row["domain"] + ".txt", "w") as f:
+                            for post_id in posts_replied_to:
+                                f.write(post_id + "\n")
+                                
+                        # wait 10 minutes
+                        time.sleep(60.0 * 10.0)
